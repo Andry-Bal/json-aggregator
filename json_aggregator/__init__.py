cc1274e747a726f1ab22f7b44b2a16900134a8f8
Andriy Balinskyy <andriy.balinskyy@gmail.com>
2023-03-16 16:13:44 +0100

    Fix import in module __init__

--- json_aggregator/__init__.py
+++ json_aggregator/__init__.py
@@ -1,2 +1,2 @@
 from json_aggregator.aggregate_json import update_agg_fns, aggregate_json_values_by_key
-from collect_json_to_csv import collect_json_to_csv
+from json_aggregator.collect_json_to_csv import collect_json_to_csv
